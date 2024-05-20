93952cb8d9dcf88972e7e369a80da4309c24ba0c
YajatKaul <mc.crafter1973@gmail.com>
2024-05-20 12:49:58 +0530

    deleted:    frontend/README.md 	modified:   frontend/package-lock.json 	modified:   frontend/package.json 	new file:   frontend/postcss.config.js 	deleted:    frontend/src/App.css 	modified:   frontend/src/App.jsx 	new file:   frontend/src/components/Cards.jsx 	new file:   frontend/src/components/InputField.jsx 	new file:   frontend/src/components/RadioButton.jsx 	new file:   frontend/src/components/TransactionForm.jsx 	new file:   frontend/src/components/ui/GridBackground.jsx 	new file:   frontend/src/components/ui/Header.jsx 	modified:   frontend/src/index.css 	modified:   frontend/src/main.jsx 	new file:   frontend/src/pages/HomePage.jsx 	new file:   frontend/src/pages/LoginPage.jsx 	new file:   frontend/src/pages/NotFoundPage.jsx 	new file:   frontend/src/pages/SignUpPage.jsx 	new file:   frontend/src/pages/TransactionPage.jsx 	new file:   frontend/src/utils/cn.js 	new file:   frontend/tailwind.config.js 	modified:   frontend/vite.config.js

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import LoginPage from "./pages/LoginPage";
+import SignUpPage from "./pages/SignUpPage";
+import TransactionPage from "./pages/TransactionPage";
+import NotFound from "./pages/NotFoundPage";
+import Header from "./components/ui/Header";
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const authUser = true;
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {authUser && <Header />}
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/transaction/:id" element={<TransactionPage />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
     </>
-  )
+  );
 }
-
-export default App
+export default App;
